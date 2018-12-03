0ed08ae6a516de53dcf044c7ab514b2b92b209fa
Jana <jana@janaread.com>
2018-12-02 22:27:23 -0500

    i think I screwed this up

--- wp-content/themes/experiment/functions.php
+++ wp-content/themes/experiment/functions.php
@@ -120,6 +120,9 @@ add_action( 'widgets_init', 'experiment_widgets_init' );
  * Enqueue scripts and styles.
  */
 function experiment_scripts() {
+
+	wp_enqueue_style('experiment-fonts', 'https://fonts.googleapis.com/css?family=PT+Serif:400,400i,700,700i|Source+Sans+Pro:400,400i,600');
+
 	wp_enqueue_style( 'experiment-style', get_stylesheet_uri() );
 
 	wp_enqueue_script( 'experiment-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
